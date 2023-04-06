5a28a60407b48d11ac1bc3e85c440eddd275ae0f
Tyler Moquin <tylmoq@gmail.com>
2023-04-06 14:26:26 -0700

    Commented Role and added constructor

--- src/scheduleme/Role.java
+++ src/scheduleme/Role.java
@@ -1,5 +1,6 @@
 /*
  * Role.java
+ * The base class for all roles in ScheduleMe.
  * 
  * VERSION HISTORY
  * v0.0.1   20230321   TAM   initial implementation
@@ -9,12 +10,29 @@
 package scheduleme;
 
 public abstract class Role {
-    private String name;
+    private String name;    // The name of the role
 
+    /**
+     * constructor
+     */
+    public Role(String aName) {
+        name = aName;
+    }
+
+    /**
+     * getName
+     * 
+     * @return String - the name of the role
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * setName
+     * 
+     * @param aName - the name of the role
+     */
     public void setName(String aName) {
         name = aName;
     }
